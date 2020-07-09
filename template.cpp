fa32c7aa62a409c5d2683c05f63378c5a08f1a0f
rahulsai1999 <noneuser2183@gmail.com>
2020-07-09 20:09:48 +0530

    template and gitignore

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Problem Desciption here
+
+/**
+ **/
+
+// Solution Class here
+
+// Driver Code here
+int main()
+{
+    return 0;
+}
