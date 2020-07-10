6c70929acfb2db4fc1c37ac77f8a17b45911fd63
rahulsai1999 <noneuser2183@gmail.com>
2020-07-10 14:08:50 +0530

    typo corrected

--- 1295.cpp
+++ 1295.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 // Problem Desciption here
+
 /**
  * Given an array nums of integers, return how many of them contain an even number of digits.
  * Input: nums = [12,345,2,6,7896]
